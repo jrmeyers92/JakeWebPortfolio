9b3fe587f971074fe941011ee23c2910e136597c
Jake Meyers <jakejake92@yahoo.com>
2020-09-15 19:50:14 -0600

    nav bar done

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function changeText() {
+    document.querySelector('.github').innerHTML = "JRMeyers92"
+}
+
+function changeTextBack() {
+    document.querySelector('.github').innerHTML = "GitHub";
+}
